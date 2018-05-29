19a3f75ae75895128b6a1aa820784732042caef6
Mijndert Stuij <mijndert@mijndertstuij.nl>
2018-05-29 08:34:55 +0200

    Fix name of image

--- run-thelounge.sh
+++ run-thelounge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name thelounge -p 9000:9000 -P --restart=always -v ~/data:/var/opt/thelounge mijndert/theloungelocal
+docker run -d --name thelounge -p 9000:9000 -P --restart=always -v ~/data:/var/opt/thelounge mijndert/thelounge-docker
